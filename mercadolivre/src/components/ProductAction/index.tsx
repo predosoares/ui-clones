5e16c8fede555a040f4c343f70e070747aaa0b56
Pedro Soares <pedrohenrique5152@gmail.com>
2020-10-09 14:54:31 -0300

    Fixing lint and adding favicons

--- mercadolivre/src/components/ProductAction/index.tsx
+++ mercadolivre/src/components/ProductAction/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
 import { Container, Condition, Row, HeartIcon, DispatchTag, PriceCard, PriceRow, InstallmentsInfo, StockStatus, MethodCard, CheckIcon, Actions, Button, Benefits, ShieldIcon } from './styles';
